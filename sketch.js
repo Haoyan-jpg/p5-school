103dde19562e279263a11b4ae267a465c002c382
Haoyan-jpg <hzhang37@tulane.edu>
2024-09-09 14:08:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,24 @@
 let ttt;
 let teleportCount = 0;
 let maxTeleports = 10;
-let angle = 0;
 let radius = 50;
 let imageVisible = true;
 let bgColor;
 let imgX, imgY;
+let score = 0;
+let gameOver = false;
+let timer = 30; 
+let imgSize = 100; 
+let difficultyIncrease = 0.95;
 
-// test commit
 function setup() {
   createCanvas(400, 400);
-  bgColor = color(240, 240, 240)
-  imgX = -20;
-  imgY = 70;
+  bgColor = color(240, 240, 240);
+  imgX = random(width - imgSize);
+  imgY = random(height - imgSize);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  setInterval(decreaseTimer, 1000);
 }
 
 function preload() {
@@ -20,73 +26,46 @@ function preload() {
 }
 
 function mouseClicked(event) {
-  if (teleportCount < maxTeleports) {
-    print("I have just clicked my mouse key!");
-
-    imgX = width / 2 + radius * cos(angle);
-    imgY = height / 2 + radius * sin(angle);
-
-    angle += TWO_PI / maxTeleports;
-    teleportCount++;
-  } else {
-    imageVisible = false;
+  if (!gameOver && imageVisible) {
+    let d = dist(mouseX, mouseY, imgX + imgSize / 2, imgY + imgSize / 2);
+    if (d < imgSize / 2) {
+      score++;
+      imgSize *= difficultyIncrease;
+      imgX = random(width - imgSize);
+      imgY = random(height - imgSize);
+    }
   }
 }
 
 function keyPressed() {
   if (key === 'c') {
-    print("pressed key C");
     bgColor = color(random(255), random(255), random(255));
+  }
+}
+
+function decreaseTimer() {
+  if (timer > 0 && !gameOver) {
+    timer--;
+  } else if (timer === 0) {
+    gameOver = true;
   }
 }
 
 function draw() {
   background(bgColor);
 
+  if (gameOver) {
+    fill(0);
+    text("Game Over!", width / 2, height / 2 - 20);
+    text("Score: " + score, width / 2, height / 2 + 20);
+    return;
+  }
 
-  fill(255, 204, 0);
-  ellipse(50, 50, 60, 60);
-
-  fill(139, 69, 19);
-  rect(300, 220, 20, 80);
-
-  fill(0, 128, 0);
-  triangle(270, 220, 310, 150, 350, 220);
-
-  fill(255, 105, 180);
-  rect(150, 250, 100, 100);
-
-  fill(255, 0, 0);
-  triangle(150, 250, 200, 200, 250, 250);
-
-  fill(255);
-  ellipse(100, 100, 50, 40);
-  ellipse(130, 90, 50, 40);
-  ellipse(160, 100, 50, 40);
-
-  fill(255, 0, 255);
-  ellipse(50, 350, 10, 10);
-  ellipse(70, 370, 10, 10);
-  ellipse(90, 360, 10, 10);
-
-  stroke(0, 128, 0);
-  line(50, 350, 50, 380);
-  line(70, 370, 70, 400);
-  line(90, 360, 90, 390);
-
-  noFill();
-  stroke(0);
-  beginShape();
-  vertex(200, 50);
-  bezierVertex(220, 70, 250, 20, 270, 70);
-  endShape();
-
-  beginShape();
-  vertex(50, 300);
-  bezierVertex(70, 320, 90, 280, 110, 320);
-  endShape();
+  fill(0);
+  text("Time: " + timer, width / 2, 30);
+  text("Score: " + score, width / 2, 70);
 
   if (imageVisible) {
-    image(ttt, imgX, imgY, 100, 100);
+    image(ttt, imgX, imgY, imgSize, imgSize);
   }
 }
