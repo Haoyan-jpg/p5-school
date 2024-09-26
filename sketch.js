2d529419cc51fe11a864d89083cb1dc0c512ebc7
Haoyan-jpg <hzhang37@tulane.edu>
2024-09-26 10:11:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,71 +1,52 @@
-let ttt;
-let teleportCount = 0;
-let maxTeleports = 10;
-let radius = 50;
-let imageVisible = true;
-let bgColor;
-let imgX, imgY;
-let score = 0;
-let gameOver = false;
-let timer = 30; 
-let imgSize = 100; 
-let difficultyIncrease = 0.95;
+let circles = [];
 
 function setup() {
-  createCanvas(400, 400);
-  bgColor = color(240, 240, 240);
-  imgX = random(width - imgSize);
-  imgY = random(height - imgSize);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  setInterval(decreaseTimer, 1000);
-}
-
-function preload() {
-  ttt = loadImage("https://static.vecteezy.com/system/resources/previews/019/773/127/original/isolated-trees-on-without-background-png.png");
-}
-
-function mouseClicked(event) {
-  if (!gameOver && imageVisible) {
-    let d = dist(mouseX, mouseY, imgX + imgSize / 2, imgY + imgSize / 2);
-    if (d < imgSize / 2) {
-      score++;
-      imgSize *= difficultyIncrease;
-      imgX = random(width - imgSize);
-      imgY = random(height - imgSize);
-    }
-  }
-}
-
-function keyPressed() {
-  if (key === 'c') {
-    bgColor = color(random(255), random(255), random(255));
-  }
-}
-
-function decreaseTimer() {
-  if (timer > 0 && !gameOver) {
-    timer--;
-  } else if (timer === 0) {
-    gameOver = true;
-  }
+  createCanvas(windowWidth, windowHeight);
+  background(240);
 }
 
 function draw() {
-  background(bgColor);
+  // Optional: Draw a fading background to create a trail effect
+  fill(240, 20);
+  noStroke();
+  rect(0, 0, width, height);
 
-  if (gameOver) {
-    fill(0);
-    text("Game Over!", width / 2, height / 2 - 20);
-    text("Score: " + score, width / 2, height / 2 + 20);
-    return;
+  // Display all circles
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].display();
+    circles[i].move();
+  }
+}
+
+// Function to create circles on mouse click
+function mousePressed() {
+  let r = random(10, 50);
+  let c = new Circle(mouseX, mouseY, r);
+  circles.push(c);
+}
+
+// Circle class
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.col = color(random(255), random(255), random(255));
   }
 
-  fill(0);
-  text("Time: " + timer, width / 2, 30);
-  text("Score: " + score, width / 2, 70);
-
-  if (imageVisible) {
-    image(ttt, imgX, imgY, imgSize, imgSize);
+  display() {
+    fill(this.col);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2);
   }
+
+  move() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+  }
+}
+
+// Resize canvas on window resize
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
